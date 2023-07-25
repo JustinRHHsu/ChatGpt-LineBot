0d603a74f58597ce5c711c741fb5ee28e056c812
Justin <justin@jingdigital.com>
2023-07-25 19:05:55 +0800

    0725-1905

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -1,3 +1,4 @@
+## api.prompt 是
 from api.prompt import Prompt
 
 import os
@@ -5,6 +6,10 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# ChatGPT 的 class 有三個 method，分別是 __init__、get_response、add_msg
+# __init__ 是初始化，會設定一些參數，例如 model、temperature、frequency_penalty、presence_penalty、max_tokens
+# get_response 是取得回應，會呼叫 openai.Completion.create 來取得回應
+# add_msg 是新增訊息，會呼叫 prompt.add_msg 來新增訊息，將使用者輸入的訊息加入 prompt 中
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
@@ -14,6 +19,29 @@ class ChatGPT:
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+
+    ## get_response 函數裡有一個 openai.Completion.create()，會呼叫 OpenAI API 來取得回應
+    ## 其中 create() 有幾個參數，分別是 model、prompt、temperature、frequency_penalty、presence_penalty、max_tokens
+    ## self.model 會帶入 OPENAI_MODEL 的環境變數，預設為 text-davinci-003
+    ## prompt object 會呼叫 generate prompt() method，generate_prompt 會將 list 中的元素組合成一個字串，並且以換行符號分隔
+    ## 再回到 get_response()裡，回存到 response 變數中，response 是一個 dictionary 的資料型態
+    ## get_response() 會回傳 response['choices'][0]['text'].strip()，這個字串就是回應的訊息
+    ## openai api 的回應格式如下
+    ## {
+    ##   "id": "cmpl-2hQJZKz5jz5n5Z5Z5Z5Z5Z5Z5",
+    ##   "object": "text_completion",
+    ##   "created": 1619268370,
+    ##   "model": "davinci:2020-05-03",
+    ##   "choices": [
+    ##     {
+    ##       "text": "Hello, how are you?",
+    ##       "index": 0,
+    ##       "logprobs": null,
+    ##       "finish_reason": "length"
+    ##     }
+    ##   ]
+    ## }
+    ## .strip() 是將字串前後的空白符號去除
 
     def get_response(self):
         response = openai.Completion.create(
--- api/prompt.py
+++ api/prompt.py
@@ -1,12 +1,19 @@
 import os
 
+
 chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
 
+# Message 最多在 List 中的數量為 20
 MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
 LANGUAGE_TABLE = {
   "zh": "嗨！",
   "en": "Hi!"
 }
+
+
+## Prompt class 的作用是將使用者輸入的訊息加入 prompt 中，並且限制 prompt 的長度
+## 其中有三個 method，分別是 __init__、add_msg、remove_msg
+## __init__ 是初始化，會設定一個 msg_list，並且將第一個訊息加入 msg_list 中
 
 class Prompt:
     def __init__(self):
@@ -14,12 +21,16 @@ class Prompt:
         self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
     
     def add_msg(self, new_msg):
+        # 如果 msg_list 的長度超過 MSG_LIST_LIMIT，就移除第一個訊息
         if len(self.msg_list) >= MSG_LIST_LIMIT:
             self.remove_msg()
         self.msg_list.append(new_msg)
 
     def remove_msg(self):
+        ## pop() 是移除 List 中的第一個元素，0 指的是第一個元素
         self.msg_list.pop(0)
 
     def generate_prompt(self):
+        ## join() 是將 List 中的元素組合成一個字串，並且以換行符號分隔
+        ## '\n'.的寫法是將換行符號加入字串中，每一個元素都會以換行符號分隔
         return '\n'.join(self.msg_list)
